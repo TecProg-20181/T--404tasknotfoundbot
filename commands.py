cf5a1df3470d8746962b0a52e42907b57734fea2
Ícaro <icarooliv2@gmail.com>
2018-04-21 21:07:43 -0300

    fixing errors and requests.py refactory
    Signed-off-by: icarooliv <icarooliv2@gmail.com>
    Signed-off-by: TPFChaos <thiagoiferreira@outlook.com>

--- commands.py
+++ commands.py
@@ -3,7 +3,6 @@ import json
 import requests
 import time
 import urllib
-
 import sqlalchemy
 
 import db
@@ -16,8 +15,8 @@ class Commands():
         db.session.add(self.task)
         db.session.commit()
         send_message("New task *TODO* [[{}]] {}".format(self.task.id, self.task.name), chat)
-
-   def renameTask(self,chat):
+        
+    def renameTask(self, chat):
        task_id = int(msg)
        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
        try:
@@ -35,32 +34,28 @@ class Commands():
        db.session.commit()
        send_message("Task {} redefined from {} to {}".format(task_id, old_text, text), chat)
 
+    def duplicateTask(self):
+        task_id = int(msg)
+        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+        try:
+            task = query.one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            send_message("_404_ Task {} not found x.x".format(task_id), chat)
+            return
 
+        dtask = Task(chat=task.chat, name=task.name, status=task.status, dependencies=task.dependencies,
+                     parents=task.parents, priority=task.priority, duedate=task.duedate)
+        db.session.add(dtask)
+ 
+        for t in task.dependencies.split(',')[:-1]:
+            qy = db.session.query(Task).filter_by(id=int(t), chat=chat)
+            t = qy.one()
+            t.parents += '{},'.format(dtask.id)
+ 
+        db.session.commit()
+        send_message("New task *TODO* [[{}]] {}".format(dtask.id, dtask.name), chat)
 
-   def duplicateTask(self):
-       task_id = int(msg)
-       query = db.session.query(Task).filter_by(id=task_id, chat=chat)
-       try:
-           task = query.one()
-       except sqlalchemy.orm.exc.NoResultFound:
-           send_message("_404_ Task {} not found x.x".format(task_id), chat)
-           return
-
-       dtask = Task(chat=task.chat, name=task.name, status=task.status, dependencies=task.dependencies,
-                    parents=task.parents, priority=task.priority, duedate=task.duedate)
-       db.session.add(dtask)
-
-       for t in task.dependencies.split(',')[:-1]:
-           qy = db.session.query(Task).filter_by(id=int(t), chat=chat)
-           t = qy.one()
-           t.parents += '{},'.format(dtask.id)
-
-       db.session.commit()
-       send_message("New task *TODO* [[{}]] {}".format(dtask.id, dtask.name), chat)
-
-
-
-   def deleteTask(self):
+    def deleteTask(self):
        task_id = int(msg)
        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
        try:
@@ -76,7 +71,6 @@ class Commands():
        db.session.commit()
        send_message("Task [[{}]] deleted".format(task_id), chat)
 
-
     def printAllTasks(self):
             task_id = int(msg)
             query = db.session.query(Task).filter_by(id=task_id, chat=chat)
@@ -88,65 +82,12 @@ class Commands():
             task.status = 'TODO'
             db.session.commit()
             send_message("*TODO* task [[{}]] {}".format(task.id, task.name), chat)
+
     def doingTask(self):
         task_id = int(msg)
         query = db.session.query(Task).filter_by(id=task_id, chat=chat)
         try:
-            task = query.one()def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
-
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
-
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += "&offset={}".format(offset)
-    js = get_json_from_url(url)
-    return js
-
-def send_message(text, chat_id, reply_markup=None):
-    text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
-    if reply_markup:
-        url += "&reply_markup={}".format(reply_markup)
-    get_url(url)
-
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-
-    return max(update_ids)
-
-def deps_text(task, chat, preceed=''):
-    text = ''
-
-    for i in range(len(task.dependencies.split(',')[:-1])):
-        line = preceed
-        query = db.session.query(Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
-        dep = query.one()
-
-        icon = '\U0001F195'
-        if dep.status == 'DOING':
-            icon = '\U000023FA'
-        elif dep.status == 'DONE':
-            icon = '\U00002611'
-
-        if i + 1 == len(task.dependencies.split(',')[:-1]):
-            line += '└── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-            line += deps_text(dep, chat, preceed + '    ')
-        else:
-            line += '├── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-            line += deps_text(dep, chat, preceed + '│   ')
-
-        text += line
-
-    return text
+            task = query.one()
         except sqlalchemy.orm.exc.NoResultFound:
             send_message("_404_ Task {} not found x.x".format(task_id), chat)
             return
--- requests.py
+++ requests.py
@@ -3,64 +3,62 @@ import requests
 import time
 import urllib
 
-import sqlalchemy
-
 import db
 from db import Task
 
 class Requests():
-    def get_url(url):
-        response = requests.get(url)
-        content = response.content.decode("utf8")
-        return content
+    def get_url(self, url):
+        self.response = requests.get(url)
+        self.content = response.content.decode("utf8")
+        return self.content
 
-    def get_json_from_url(url):
-        content = get_url(url)
-        js = json.loads(content)
-        return js
+    def get_json_from_url(self, url):
+        self.content = get_url(url)
+        self.js = json.loads(content)
+        return self.js
 
-    def get_updates(offset=None):
-        url = URL + "getUpdates?timeout=100"
+    def get_updates(self, offset=None):
+        self.url = URL + "getUpdates?timeout=100"
         if offset:
-            url += "&offset={}".format(offset)
-        js = get_json_from_url(url)
-        return js
+            self.url += "&offset={}".format(offset)
+        self.js = get_json_from_url(url)
+        return self.js
 
-    def send_message(text, chat_id, reply_markup=None):
-        text = urllib.parse.quote_plus(text)
-        url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    def send_message(self, text, chat_id, reply_markup=None):
+        self.text = urllib.parse.quote_plus(text)
+        self.url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
         if reply_markup:
-            url += "&reply_markup={}".format(reply_markup)
-        get_url(url)
+            self.url += "&reply_markup={}".format(reply_markup)
+        get_url(self.url)
 
-    def get_last_update_id(updates):
-        update_ids = []
-        for update in updates["result"]:
-            update_ids.append(int(update["update_id"]))
+    def get_last_update_id(self, updates):
+        self.update_ids = []
+        for self.update in updates["result"]:
+            self.update_ids.append(int(self.update["update_id"]))
 
-        return max(update_ids)
+        return max(self.update_ids)
 
-    def deps_text(task, chat, preceed=''):
-        text = ''
+    def deps_text(self, task, chat, preceed=''):
+        self.text = ''
 
-        for i in range(len(task.dependencies.split(',')[:-1])):
-            line = preceed
-            query = db.session.query(Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
-            dep = query.one()
+        for self.i in range(len(task.dependencies.split(',')[:-1])):
+            self.line = preceed
+            self.query = db.session.query(Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
+            self.dep = query.one()
 
-            icon = '\U0001F195'
-            if dep.status == 'DOING':
-                icon = '\U000023FA'
-            elif dep.status == 'DONE':
-                icon = '\U00002611'
+            self.icon = '\U0001F195'
+            if self.dep.status == 'DOING':
+                self.icon = '\U000023FA'
+            elif self.dep.status == 'DONE':
+                self.icon = '\U00002611'
 
-            if i + 1 == len(task.dependencies.split(',')[:-1]):
-                line += '└── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-                line += deps_text(dep, chat, preceed + '    ')
+            if self.i + 1 == len(task.dependencies.split(',')[:-1]):
+                self.line += '└── [[{}]] {} {}\n'.format(self.dep.id, self.icon, self.dep.name)
+                self.line += deps_text(self.dep, chat, preceed + '    ')
             else:
-                line += '├── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-                line += deps_text(dep, chat, preceed + '│   ')
+                self.line += '├── [[{}]] {} {}\n'.format(self.dep.id, self.icon, self.dep.name)
+                self.line += deps_text(self.dep, chat, preceed + '│   ')
 
-            text += line
+            self.text += self.line
 
-        return text
+        return self.text
